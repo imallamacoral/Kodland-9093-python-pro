5fd4028a1313889e808f3ee4cf498fd7b67ab5bd
imallamacoral <i.mallama.coral@gmail.com>
2025-10-14 20:36:44 -0500

    Add files via upload

---
+++ GeneradorContraseñas.py
@@ -0,0 +1,3 @@
+characteres = "+-/*!&$#?=@abcdefghijklnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+characterNumber = int(input("introduce la longitud de la contraseña "))
+print(characterNumber)
